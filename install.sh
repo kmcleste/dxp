42f94a96c6c0391afaa04221e3ec8d68beaa42a3
Kyle McLester <kmcleste@uncc.edu>
2023-06-24 23:52:10 -0400

    fix zshrc modifications

--- install.sh
+++ install.sh
@@ -9,48 +9,66 @@ else
 fi
 
 # Install python build dependencies
-brew install openssl readline sqlite3 xz zlib tcl-tk
-
-# Install poetry
-if ! command -v poetry &> /dev/null
-then
-    curl -sSL https://install.python-poetry.org | python3 -
-    echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.zshrc
-else
-    poetry self update
-fi
+brew install openssl readline sqlite3 xz zlib tcl-tk ca-certificates
 
 # Install pyenv
 if ! command -v pyenv &> /dev/null
 then
     brew install pyenv
-    echo export PYENV_ROOT="\$HOME/.pyenv" >> ~/.zshrc
-    echo export PATH="\$PYENV_ROOT/bin:\$PATH" >> ~/.zshrc
-    echo eval "$(pyenv init -)" >> ~/.zshrc
-    echo export PYENV_ROOT="\$HOME/.pyenv" >> ~/.zprofile
-    echo export PATH="\$PYENV_ROOT/bin:\$PATH" >> ~/.zprofile
-    echo eval "$(pyenv init -)" >> ~/.zprofile
+    if ! grep -Fxq "export PYENV_ROOT=\$HOME/.pyenv" ~/.zshrc &> /dev/null
+    then
+        echo export PYENV_ROOT="\$HOME/.pyenv" >> ~/.zshrc 
+    fi
+    if ! grep -Fxq "export PATH=\$PYENV_ROOT/bin:\$PATH" ~/.zshrc &> /dev/null
+    then
+        echo export PATH="\$PYENV_ROOT/bin:\$PATH" >> ~/.zshrc
+    fi
+    if ! grep -Fxq 'eval "$(pyenv init -)"' ~/.zshrc &> /dev/null
+    then
+        echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+    fi
+    if ! grep -Fxq "export PYENV_ROOT=\$HOME/.pyenv" ~/.zprofile &> /dev/null
+    then
+        echo export PYENV_ROOT="\$HOME/.pyenv" >> ~/.zprofile
+    fi
+    if ! grep -Fxq "export PATH=\$PYENV_ROOT/bin:\$PATH" ~/.zprofile &> /dev/null
+    then
+        echo export PATH="\$PYENV_ROOT/bin:\$PATH" >> ~/.zprofile
+    fi
+    if ! grep -Fxq 'eval "$(pyenv init -)"' ~/.zprofile &> /dev/null
+    then
+        echo 'eval "$(pyenv init -)"' >> ~/.zprofile
+    fi
 else
     brew upgrade pyenv
+fi
+
+# Install poetry
+if ! command -v poetry &> /dev/null
+then
+    curl -sSL https://install.python-poetry.org | python3 -
+    if ! grep -Fxq "export PATH=\$HOME/.local/bin:\$PATH" ~/.zshrc &> /dev/null
+    then
+        echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.zshrc
+    fi
+else
+    poetry self update
 fi
 
 # Install tfenv
 if ! command -v tfenv &> /dev/null
 then
     brew install tfenv
+    if ! grep -Fxq "export PATH=\$HOME/.tfenv/bin:\$PATH" ~/.zshrc &> /dev/null
+    then
+        echo export PATH="\$HOME/.tfenv/bin:\$PATH" >> ~/.zshrc
+    fi
+    if ! grep -Fxq "export PATH=\$HOME/.tfenv/bin:\$PATH" ~/.zprofile &> /dev/null
+    then
+        echo export PATH="\$HOME/.tfenv/bin:\$PATH" >> ~/.zprofile
+    fi
     tfenv install
     tfenv use
-    echo export PATH="\$HOME/.tfenv/bin:\$PATH" >> ~/.zshrc
-    echo export PATH="\$HOME/.tfenv/bin:\$PATH" >> ~/.zprofile
 else
     brew upgrade tfenv
-fi
-
-# Install cookiecutter
-if ! command -v cookiecutter &> /dev/null
-then
-    brew install cookiecutter
-    echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.zshrc # TODO: Check if already exists in zsh
-else
-    brew upgrade cookiecutter
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -4,23 +4,25 @@
 if command -v poetry &> /dev/null
 then
     curl -sSL https://install.python-poetry.org | python3 - --uninstall
-    # TODO: Update zshrc poetry path
+    sed -i -e '/export PATH=\$HOME\/.local\/bin:\$PATH/d' ~/.zshrc
 fi
 
 # Uninstall pyenv
 if command -v pyenv &> /dev/null
 then
     brew uninstall pyenv
+    sed -i -e '/export PYENV_ROOT=\$HOME\/.pyenv/d' ~/.zshrc
+    sed -i -e '/export PATH=\$PYENV_ROOT\/bin:\$PATH/d' ~/.zshrc
+    sed -i -e '/eval "\$(pyenv init -)"/d' ~/.zshrc
+    sed -i -e '/export PYENV_ROOT=\$HOME\/.pyenv/d' ~/.zprofile
+    sed -i -e '/export PATH=\$PYENV_ROOT\/bin:\$PATH/d' ~/.zprofile
+    sed -i -e '/eval "\$(pyenv init -)"/d' ~/.zprofile
 fi
 
 # Uninstall tfenv
 if command -v tfenv &> /dev/null
 then
     brew uninstall tfenv
-fi
-
-# Uninstall cookiecutter
-if command -v cookiecutter &> /dev/null
-then
-    brew uninstall cookiecutter
+    sed -i -e '/export PATH=\$HOME\/.tfenv\/bin:\$PATH/d' ~/.zshrc
+    sed -i -e '/export PATH=\$HOME\/.tfenv\/bin:\$PATH/d' ~/.zprofile
 fi
